09aca0ef7c8278c9cf1cbf6365e62c800434fc60
fatima <fatimalvcruz@gmail.com>
2021-07-31 19:48:38 +1000

    Add versioning

--- index.php
+++ index.php
@@ -96,6 +96,6 @@
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
         -->      
-        <script src="js/main.js" type="text/javascript" ></script>
+        <script src="js/main.js?v=1" type="text/javascript" ></script>
     </body>
 </html>
